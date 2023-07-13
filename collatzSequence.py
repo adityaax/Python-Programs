f1adb28fc41cc22dc541a1fe4a4b2f4e179b7b66
Aditya Saxena <98630015+adityaax@users.noreply.github.com>
2023-07-13 22:56:41 +0530

    Add files via upload

---
+++ collatzSequence.py
@@ -0,0 +1,17 @@
+def even(number):
+    if number%2==0:
+        return True
+    
+def collatz(number):
+    n=number
+    global nam
+    if even(n):
+        print(n//2)
+        nam=n//2
+    else:
+        print(3*n+1)
+        nam=3*n+1
+
+nam=int(input("Enter any integer- "))
+while nam!=1:
+    collatz(nam)
