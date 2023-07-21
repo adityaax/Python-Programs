6fb0df6f4d3273d5fa9c90c6cca891947753c7de
Aditya Saxena <98630015+adityaax@users.noreply.github.com>
2023-07-21 23:16:35 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,5 +1,25 @@
+#! python3
+# regex.py - learning regex
+
 import re
+'''
 text='my number is 34-675 please note it...'
 regex=re.compile(r'\d\d-\d\d\d')
+result=regex.search(text)
+print(result.group())
+
+text='separate +91-378 as country code and number'
+regex=re.compile(r'(\+\d\d)-(\d\d\d)')
+result=regex.search(text)
+print(result.group()) #print(result.group(1)) , print(result.group(2))
+
+text='my name is hena sometimes called montena'
+regex=re.compile(r'hena|montena')
+result=regex.search(text)
+print(result.group())
+'''
+
+text='batman batwoman batchild'
+regex=re.compile(r'bat(man|woman|child)')
 result=regex.search(text)
 print(result.group())
