9ab475453c20422396dc9f60081e8ea09cc634bd
Aditya Saxena <98630015+adityaax@users.noreply.github.com>
2023-07-23 10:50:11 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -17,9 +17,40 @@ text='my name is hena sometimes called montena'
 regex=re.compile(r'hena|montena')
 result=regex.search(text)
 print(result.group())
-'''
 
 text='batman batwoman batchild'
 regex=re.compile(r'bat(man|woman|child)')
 result=regex.search(text)
 print(result.group())
+
+text = 'Where is the batwowowoman?'
+regex = re.compile(r'bat(wo)*man') # 0 or more occurence
+result = regex.search(text)
+print(result.group())
+
+regex1 = re.compile(r'bat(wo)+man') # 1 or more occurence
+result = regex1.search(text)
+print(result.group())
+
+text = 'where is the batman'
+regex2 = re.compile(r'bat(wo)?man') #0 or 1 occurence
+result = regex2.search(text)
+print(result == None)
+#print(result.group())
+
+text = 'where is the batwowowowowoman'
+regex = re.compile(r'bat(wo){3,5}man') # greedy(return longest string)
+result = regex.search(text)
+print(result.group())
+
+text = 'wowowowowo'
+regex = re.compile(r'{3,5}?') #non greedy
+result = regex.search(text)
+print(result.group())
+
+text = 'my work number is 234-645-645 and house number is 987-234-765'
+regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d')
+result = regex.findall(text)
+print(result)
+
+'''
